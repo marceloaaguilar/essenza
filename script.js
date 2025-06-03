deeda94529800f3d1c4a26906860a44a7398ae71
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2025-06-02 23:56:44 -0300

    fixes

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".hero-swiper", {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
